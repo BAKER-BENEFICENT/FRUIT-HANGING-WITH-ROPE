ec2910e5cba6d19445a188360823fff03808277c
BAKER-BENEFICENT <94804848+BAKER-BENEFICENT@users.noreply.github.com>
2022-03-28 21:04:27 +0100

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,24 @@
+    class Ground
+{
+    constructor(x, y, width, height)
+    {
+        var options = 
+        {
+            isStatic : true
+        }
+
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.w = width;
+        this.h = height;
+        World.add(world, this.body);
+    }
+
+    show()
+    {
+        push();
+        rectMode(CENTER);
+        fill("brown");
+        rect(this.body.position.x, this.body.position.y, this.w, this.h);
+        pop();
+    }
+}
